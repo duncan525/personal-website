2b38386bbc95cf9872f5920cfe1cdcda79b354b6
duncan525 <duncanluke525@gmail.com>
2025-10-28 21:19:26 -0400

    page_styles --> projects_styles; credits script added
    1. I give up on fighting my poorly thought out naming conventions. it shall remain projects_styles for eternity
    2. added a credits.js script to give the buttons a use (can now page through content)

---
+++ pages/credits.js
@@ -0,0 +1,54 @@
+var index = 0;
+const MAX_INDEX = 2;
+
+var pictures = document.getElementsByClassName("picture");
+var current_picture = pictures[index];
+
+var creator_titles = document.getElementsByClassName("creator_title");
+var creator_contents = document.getElementsByClassName("creator_content");
+var current_creator_title = creator_titles[index];
+var current_creator_content = creator_contents[index];
+
+var credits_titles = document.getElementsByClassName("credits_title");
+var credits_contents = document.getElementsByClassName("credits_content");
+var current_credits_title = credits_titles[index];
+var current_credits_content = credits_contents[index];
+
+var text_titles = document.getElementsByClassName("text_title");
+var text_contents = document.getElementsByClassName("text_contents");
+var current_text_title = text_titles[index];
+var current_text_content = text_contents[index];
+
+var elements = [pictures, creator_titles, creator_contents, credits_titles, credits_contents,
+                text_titles, text_contents]
+var current_elements = [current_picture, current_creator_title, current_creator_content,
+                    current_credits_title, current_credits_content, current_text_title,
+                    current_text_content]
+
+var button_left = document.getElementById("left_button");
+button_left.addEventListener("click", function(){
+    advance_credits(-1);
+})
+
+var button_right = document.getElementById("right_button");
+button_right.addEventListener("click", function(){
+    advance_credits(1);
+})
+
+function advance_credits(direction) {
+    index += direction;
+    if (index < 0)
+        index = MAX_INDEX;
+    else if (index > MAX_INDEX)
+        index = 0;
+
+    console.log(index);
+
+    for (var i = 0; i < elements.length; i++) {
+        current_elements[i].classList.add("hide");
+        current_elements[i] = elements[i][index];
+        current_elements[i].classList.remove("hide");
+    }
+}
+
+
